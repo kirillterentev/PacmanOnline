b1fa3b9026920a834b604b7e94b9e4d21f4f5341
Terentev Kirill <mowder96@gmail.com>
2020-05-24 23:28:26 +0500

    server project added

---
+++ PacmanServer/Program/Pacman.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using ProtoBuf;
+
+namespace PacmanServer
+{
+	[Serializable]
+	[ProtoContract]
+	public class Joystick
+	{
+		[ProtoMember(1)]
+		public int axisX;
+
+		[ProtoMember(2)]
+		public int axisY;
+	}
+
+	[Serializable]
+	[ProtoContract]
+	public class PlayerInfo
+	{
+		[ProtoMember(1)]
+		public string Nickname;
+
+		[ProtoMember(2)]
+		public string Color;
+
+		[ProtoMember(3)]
+		public Coord Coord;
+	}
+
+	[Serializable]
+	[ProtoContract]
+	public class Coord
+	{
+		[ProtoMember(1)]
+		public int X;
+
+		[ProtoMember(2)]
+		public int Y;
+	}
+
+	[Serializable]
+	[ProtoContract]
+	public class GameField
+	{
+		[ProtoMember(1)]
+		public Coord Size;
+
+		[ProtoMember(2)]
+		public List<Coord> Cells;
+	}
+
+}
+
---
+++ PacmanServer/Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Program")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Program")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("86b2c204-3a9c-4ddd-81d5-b318bd9edc6c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
