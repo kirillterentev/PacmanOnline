7c1012c8b69bd5180b19a8f1318116de0f747cf4
Terentev Kirill <mowder96@gmail.com>
2020-05-27 01:06:18 +0500

    Интерфейс подключения игрока

---
+++ Assets/Scripts/ConnectWindow.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectWindow : MonoBehaviour
+{
+	[SerializeField]
+	private GameController gameController;
+	[SerializeField]
+	private InputField inputField;
+	[SerializeField]
+	private ToggleGroup toggleGroup;
+	[SerializeField]
+	private Button connectButton;
+
+	private string color;
+
+	private void Start()
+	{
+		connectButton.onClick.AddListener(ConnectClient);
+		toggleGroup.SetAllTogglesOff();
+	}
+
+	private void ConnectClient()
+	{
+		if (inputField.text == string.Empty)
+		{
+			Debug.Log("Введите имя");
+			return;
+		}
+
+		if (!toggleGroup.AnyTogglesOn())
+		{
+			Debug.Log("Выберите цвет");
+			return;
+		}
+
+		gameController.ConnectToServer(inputField.text, color);
+		gameObject.SetActive(false);
+	}
+
+	public void SetColor(string color)
+	{
+		this.color = color;
+	}
+
+	private void OnDestroy()
+	{
+		connectButton.onClick.RemoveAllListeners();
+	}
+}
